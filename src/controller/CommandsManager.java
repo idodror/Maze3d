544106c1bf0876b0ef41e9766379f6b6cb9db9a3
gal <gal@10.0.0.9>
2016-09-04 22:15:53 +0300

    JAVADOC TO ALL PACKAGE CONTROLLER

--- src/controller/CommandsManager.java
+++ src/controller/CommandsManager.java
@@ -3,20 +3,33 @@ package controller;
 import java.util.HashMap;
 import model.Model;
 import view.View;
-
+/**
+ * CommandsManager
+ * this is the CommandsManager that will control all the mvc
+ * it have Model, View and HashMap<String, Command>
+ */
 public class CommandsManager {
 	
 	private Model model;
 	private View view;
 	private HashMap<String, Command> commandMap;
-
+	
+	/**
+	 * Contractor
+	 * @param Model, View
+	 * at the end of the Contractor we will put all the command in the hashmap
+	 */
 	public CommandsManager(Model model, View view) {
 		this.model = model;
 		this.view = view;
 		this.commandMap = new HashMap<String, Command>();
 		putAllCommandsInHashMap();
 	}
-
+	
+	/**
+	 * putAllCommandsInHashMap
+	 * add all the command to the hash map
+	 */
 	private void putAllCommandsInHashMap() {
 		this.commandMap.put("u", new UpCommand());
 		this.commandMap.put("d", new DownCommand());
@@ -34,110 +47,253 @@ public class CommandsManager {
 		this.commandMap.put("display_solution", new DisplaySolution());
 	}
 	
+	/**
+	 * class UpCommand
+	 * the command that will be up in the cli
+	 *implements Command
+	 */
 	class UpCommand implements Command {
 
+		/**
+		 * doCommand
+		 * call the go up of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.goUp(args);
 		}
 	}
 	
+	/**
+	 * class DownCommand
+	 * the command that will be down in the cli
+	 *implements Command
+	 */
 	class DownCommand implements Command {
 
+		/**
+		 * doCommand
+		 * call the go down of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.goDown(args);
 		}
 	}
 	
+	/**
+	 * class RightCommand
+	 * the command that will be right in the cli
+	 *implements Command
+	 */
 	class RightCommand implements Command {
 
+		/**
+		 * doCommand
+		 * call the go Right of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.goRight(args);
 		}
 	}
 	
+	/**
+	 * class LeftCommand
+	 * the command that will be left in the cli
+	 *implements Command
+	 */
 	class LeftCommand implements Command {
 
+		/**
+		 * doCommand
+		 * call the go Left of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.goLeft(args);
 		}
 	}
 	
+	/**
+	 * class ForwardCommand
+	 * the command that will be forward in the cli
+	 *implements Command
+	 */
 	class ForwardCommand implements Command {
 
+		/**
+		 * doCommand
+		 * call the go Forward of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.goForward(args);
 		}
 	}
 	
+	/**
+	 * class BackwardCommand 
+	  * the command that will be backward in the cli
+	 *implements Command
+	 */
 	class BackwardCommand implements Command {
 
+		/**
+		 * doCommand
+		 * call the go Backward of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.goBackward(args);
 		}
 	}
 	
+	/**
+	 * class GenerateMaze 
+	 * the command that will be generate_maze in the cli
+	 *implements Command
+	 */
 	class GenerateMaze implements Command {
 
+		/**
+		 * doCommand
+		 * call the generateMaze of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.generateMaze(args);
 		}
 	}
+	/**
+	 * class DisplayMaze 
+	 * the command that will be display in the cli
+	 *implements Command
+	 */
 	
 	class DisplayMaze implements Command {
 
+		/**
+		 * doCommand
+		 * call the displayMaze of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.displayMaze(args);	
 		}
 	}
 	
+	/**
+	 * class DisplayFilesInPath 
+	 *  * the command that will be dir in the cli
+	 *implements Command
+	 */
 	class DisplayFilesInPath implements Command {
 
+		/**
+		 * doCommand
+		 * call the displayFilesInPath of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.displayFilesInPath(args);
 		}
 	}
 	
+	/**
+	 * class DisplayCrossSection 
+	 *  * the command that will be display_crocss_section in the cli
+	 *implements Command
+	 */
 	class DisplayCrossSection implements Command {
 
+		/**
+		 * doCommand
+		 * call the displayCrossSection of model
+		 * @param String[]
+		 */		
 		@Override
 		public void doCommand(String[] args) {
 			model.displayCrossSection(args);
 		}
 	}
 	
+	/**
+	 * class SaveMaze 
+	 * the command that will be save_maze in the cli
+	 *implements Command
+	 */
 	class SaveMaze implements Command {
 
+		/**
+		 * doCommand
+		 * call the saveMaze of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.saveMaze(args);
 		}
 	}
+	
+	/**
+	 * class LoadMaze 
+	 * the command that will be load_maze in the cli
+	 *implements Command
+	 */
 	class LoadMaze implements Command {
 
+		/**
+		 * doCommand
+		 * call the LoadMaze of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.loadMaze(args);
 		}
 	}
+	
+	/**
+	 * class Solve 
+	 * the command that will be Solve-the maze in the cli
+	 *implements Command
+	 */
 	class Solve implements Command{
-
+		
+		/**
+		 * doCommand
+		 * call the solve of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.solve(args);			
 		}
 		
 	}
+	
+	/**
+	 * class DisplaySolution 
+	 * the command that will be display_solution in the cli
+	 *implements Command
+	 */
 	class DisplaySolution implements Command{
 
+		/**
+		 * doCommand
+		 * call the displaySolution of model
+		 * @param String[]
+		 */
 		@Override
 		public void doCommand(String[] args) {
 			model.displaySolution(args);			
@@ -145,14 +301,25 @@ public class CommandsManager {
 		
 	}
 
+	/**
+	 * getView
+	 * @return View
+	 */
 	public View getView() {
 		return this.view;
 	}
 
+	/**
+	 * getCommandMap
+	 * @return HashMap<String, Command>
+	 */
 	public HashMap<String, Command> getCommandMap() {
 		return this.commandMap;
 	}
-
+/**
+ * setModelAndView
+ * @param Model ,View
+ */
 	public void setModelAndView(Model model, View view) {
 		this.model = model;
 		this.view = view;
--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -4,25 +4,48 @@ import algorithms.mazeGenerators.Position;
 import model.Model;
 import view.View;
 
+/**
+ * This is the controller layer of the MVC
+ * Makes all the management  of the game
+ * Holds the model,view and commandsManager
+ */
 public class MyController implements Controller {
 	
 	private Model model;
 	private View view;
 	private CommandsManager commandsManager;
 	
+	/**
+	 * Constructor
+	 */
 	public MyController() {
 		commandsManager = new CommandsManager(this.model, this.view);
 	}
-
+	
+	/**
+	 * executeCommand
+	 * call the do command of command
+	 * @param Command, String[]
+	 */
 	@Override
 	public void executeCommand (Command cmd, String[] args) {
 		cmd.doCommand(args);
 	}
-
+	
+	/**
+	 * displayPosition
+	 * call the displayPosition of view 
+	 * @param Position
+	 */
 	@Override
 	public void displayPosition(Position pos) {
 		this.view.displayPosition(pos);
 	}
+	/**
+	 * setModelAndView
+	 * set the model ,view and the commend manager
+	 * @param Model ,View
+	 */
 
 	@Override
 	public void setModelAndView(Model model, View view) {
@@ -32,6 +55,11 @@ public class MyController implements Controller {
 		this.commandsManager.setModelAndView(model, view);
 	}
 
+	/**
+	 * printToOutputStream
+	 * call printToOutputStream of view
+	 * @param String
+	 */
 	@Override
 	public void printToOutputStream(String out) {
 		this.view.printToOutputStream(out);
